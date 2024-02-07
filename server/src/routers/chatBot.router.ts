2588ad8b57d4facc8097ddc90375781eabff5dae
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-07 18:29:14 +0300

    Initial Commit

--- server/src/routers/chatBot.router.ts
+++ server/src/routers/chatBot.router.ts
@@ -1,8 +1,14 @@
 import express, { Router } from 'express';
-import { chatWithBot, newChat } from '../controllers/chatBot.controller';
+import {
+  chatWithBot,
+  generateImages,
+  newChat,
+} from '../controllers/chatBot.controller';
 
 const BotRouter: Router = express.Router();
 
-BotRouter.post('/chatbot', chatWithBot).post('/new', newChat);
+BotRouter.post('/chatbot', chatWithBot)
+  .post('/new', newChat)
+  .post('/generateImg', generateImages);
 console.log('chatbot router loaded');
 export default BotRouter;
