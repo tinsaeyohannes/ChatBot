0be50fe28a5b2b053907349f213bb710f56756a7
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-07 17:22:30 +0300

    ⚙️chore(controller) [modify chatBot.controller.ts]: Update code to handle conversation history and new chat requests
    In this commit, the code in the chatBot.controller.ts file has been modified to handle conversation history and new chat requests. The following changes were made:
    
    - Added import statement for the ConversationHistory.mongo model
    - Renamed the chatWithBot function to newChat
    - Updated the model parameter in the openai.beta.chat.completions.stream function to 'gpt-3.5-turbo-16k'
    - Refactored the for loop to iterate over the stream and retrieve the bot's response
    - Created a new instance of the HistoryModel and saved it to store the conversation history
    - Updated the response to return the bot's message content
    - Modified the chatWithBot function to handle the message and id parameters from the request body
    - Added validations for the presence of id and message parameters
    - Fetched the conversationHistory using the id parameter and performed error handling if it is not found
    - Created a history array by mapping the conversationHistory.history and appending the user's message
    - Updated the model parameter in the openai.beta.chat.completions.stream function to 'gpt-3.5-turbo

--- server/src/controllers/chatBot.controller.ts
+++ server/src/controllers/chatBot.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import OpenAI from 'openai';
+import HistoryModel from '../models/ConversationHistory.mongo';
 
 console.log(process.env.OPENAI_API_KEY);
 
@@ -9,7 +10,7 @@ const openai = new OpenAI({
     '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', // This is the default and can be omitted
 });
 
-const chatWithBot = async (req: Request, res: Response) => {
+const newChat = async (req: Request, res: Response) => {
   const { message }: { message: string } = req.body;
 
   if (!message) {
@@ -18,31 +19,96 @@ const chatWithBot = async (req: Request, res: Response) => {
 
   try {
     const stream = openai.beta.chat.completions.stream({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: message }],
+      model: 'gpt-3.5-turbo-16k',
+      messages: [
+        {
+          role: 'user',
+          content: message,
+        },
+      ],
       stream: true,
     });
-
-    // stream.on('content', (delta, snapshot) => {
-    //   process.stdout.write(delta);
-
-    //   if (snapshot) {
-    //     process.stdout.write(snapshot);
-    //   }
-    // });
-
-    // or, equivalently:
 
     for await (const chunk of stream) {
       const botStream = chunk.choices[0]?.delta?.content || '';
       process.stdout.write(botStream || '');
-      res.write(botStream);
     }
-    res.end();
+
+    const newChat = new HistoryModel({
+      history: [
+        {
+          chatName: '',
+          user: message,
+          bot: (await stream.finalChatCompletion()).choices[0].message.content,
+        },
+      ],
+    });
+
+    await newChat.save();
+
+    res
+      .status(200)
+      .json((await stream.finalChatCompletion()).choices[0].message.content);
   } catch (error) {
     console.error((error as Error).message);
     res.status(500).json('Internal Server Error!');
   }
 };
 
-export { chatWithBot };
+const chatWithBot = async (req: Request, res: Response) => {
+  const { message, id }: { message: string; id: string } = req.body;
+
+  if (!id) {
+    return res.status(400).json('Please enter an chat id');
+  }
+  if (!message) {
+    return res.status(400).json('Please enter a message');
+  }
+  const conversationHistory = await HistoryModel.findById(id);
+
+  if (!conversationHistory) {
+    return res.status(404).json('Empty History!');
+  }
+
+  const history = conversationHistory.history.map((doc) => ({
+    role: doc.user,
+    content: doc.bot,
+  }));
+
+  history.push({
+    role: 'user',
+    content: message as string,
+  });
+
+  try {
+    const stream = openai.beta.chat.completions.stream({
+      model: 'gpt-3.5-turbo',
+      messages: history.map((msg) => ({
+        role: 'user',
+        content: msg.role + msg.content,
+      })),
+      stream: true,
+    });
+
+    for await (const chunk of stream) {
+      const botStream = chunk.choices[0]?.delta?.content || '';
+      process.stdout.write(botStream || '');
+    }
+
+    conversationHistory.history.push({
+      chatName: '',
+      user: message,
+      bot: (await stream.finalChatCompletion()).choices[0].message.content,
+    });
+
+    await conversationHistory.save();
+    res
+      .status(200)
+      .json((await stream.finalChatCompletion()).choices[0].message);
+  } catch (error) {
+    console.error((error as Error).message);
+    res.status(500).json('Internal Server Error!');
+  }
+};
+
+export { chatWithBot, newChat };
