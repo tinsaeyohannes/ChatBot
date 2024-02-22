6f2fa0ff7fdcde99bae0a0f1fb03ee44f0794226
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-22 12:25:08 +0300

    🛠️refactor(eslint) Updated eslint configuration to include rules for prettier formatting and disable unnecessary react rules.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
