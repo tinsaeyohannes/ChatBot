694bc98fde131fae8b07f0fa14f0d41c6e763218
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-22 20:41:00 +0300

    🎨 Improve structure / format of the code.
    - Improve the structure and format of the code in the App.tsx file.
    - Add import statements for necessary- Update the component names and their respective props.
     the use device is in dark mode.
    - Update the useEffect hooks to set the dark.
    
     code in.tsx file.
    - Add import statements for necessary dependencies.
    - Update the names and their respective props.
    - Add a the DrawerBarContent.
    - Use the useColorScheme hook to determine if the device is in dark mode.
    - Update the useEffect hooks to set the dark mode and system navigation bar color.
    - Update the Drawer.Navigator options and screenOptions.
    - Update the.Screen options and children components.

--- client/App.tsx
+++ client/App.tsx
@@ -1,36 +1,82 @@
 import * as React from 'react';
-import {Button, View} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  type DrawerContentComponentProps,
+} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
-
-function HomeScreen({navigation}: {navigation: any}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({navigation}: {navigation: any}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+import SystemNavigationBar from 'react-native-system-navigation-bar';
+import ChatScreen from './src/screens/ChatScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import DrawerBarContent from './src/drawerBar/DrawerBarContent';
+import {useColorScheme} from 'react-native';
+import {userStore} from './src/store/useStore';
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
+function DrawerBarContentComponent(
+  props: DrawerContentComponentProps,
+): React.JSX.Element {
+  return <DrawerBarContent {...props} />;
+}
+
+function App() {
+  const {isDarkMode, setIsDarkMode} = userStore(state => ({
+    isDarkMode: state.isDarkMode,
+    setIsDarkMode: state.setIsDarkMode,
+  }));
+
+  const isDark = useColorScheme() === 'dark';
+
+  React.useEffect(() => {
+    setIsDarkMode(isDark);
+    console.log(isDarkMode);
+  }, [isDark, isDarkMode, setIsDarkMode]);
+
+  React.useEffect(() => {
+    SystemNavigationBar.setNavigationColor(isDark ? '#101010' : '#E5E5E5');
+    SystemNavigationBar.setNavigationBarContrastEnforced(true);
+  }, [isDark]);
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Navigator
+        initialRouteName="Chat"
+        screenOptions={{
+          drawerType: 'back',
+          headerStyle: {
+            backgroundColor: 'blue',
+            shadowColor: 'transparent',
+            elevation: 0,
+            borderBottomWidth: 0,
+          },
+
+          headerTintColor: 'white',
+          headerTransparent: true,
+          keyboardDismissMode: 'on-drag',
+          drawerStyle: {
+            backgroundColor: '#fff',
+          },
+        }}
+        drawerContent={DrawerBarContentComponent}>
+        <Drawer.Screen
+          name="Chat"
+          children={() => <ChatScreen isDarkMode={isDarkMode} />}
+          options={{
+            title: 'Chat Bot',
+            headerShown: false,
+          }}
+        />
+        <Drawer.Screen
+          name="Profile"
+          children={() => <ProfileScreen isDarkMode={isDarkMode} />}
+          options={{
+            swipeEnabled: false,
+            headerShown: false,
+            drawerLabel: 'Profile',
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
