1400c8006e13e8e52c43d052aac97966d22ebc04
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-23 08:50:12 +0300

    🚀 feat(chatbot): Added routes to get all chat history and get chat by ID; reorganized routes in chatBot.router.ts

--- server/src/routers/chatBot.router.ts
+++ server/src/routers/chatBot.router.ts
@@ -1,14 +1,18 @@
 import express, { Router } from 'express';
 import {
   chatWithBot,
-  generateImages,
+  getAllChatHistory,
+  getChatById,
   newChat,
 } from '../controllers/chatBot.controller';
+import { generateImages } from '../controllers/imageGenerator.controller';
 
 const BotRouter: Router = express.Router();
 
 BotRouter.post('/chatbot', chatWithBot)
   .post('/new', newChat)
-  .post('/generateImg', generateImages);
-console.log('chatbot router loaded');
+  .post('/generateImg', generateImages)
+  .get('/all', getAllChatHistory)
+  .get('/:id', getChatById);
+
 export default BotRouter;
