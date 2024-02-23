3fef23b03e50a22156522d453df743a6760a5aae
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-23 21:49:58 +0300

    🚀feat(chatBot): Added functionality to delete chat and chat messages - Added new routes for deleting chat and chat messages in the ChatBot router file.

--- server/src/routers/chatBot.router.ts
+++ server/src/routers/chatBot.router.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 import {
   chatWithBot,
+  deleteChat,
   getAllChatHistory,
   getChatById,
   newChat,
+  deleteMessage,
 } from '../controllers/chatBot.controller';
 import { generateImages } from '../controllers/imageGenerator.controller';
 
@@ -13,6 +15,8 @@ BotRouter.post('/chatbot', chatWithBot)
   .post('/new', newChat)
   .post('/generateImg', generateImages)
   .get('/all', getAllChatHistory)
-  .get('/:id', getChatById);
+  .get('/:id', getChatById)
+  .delete('/delete/:id', deleteChat)
+  .delete('/delete/message/:id', deleteMessage);
 
 export default BotRouter;
