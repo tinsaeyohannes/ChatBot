551a7d5e3060855c4823db70e08c18c7dd2ce71b
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-24 22:35:11 +0300

    🚀feat(useChatStoreTypes) Added new types for chat conversation details Explanation: Added new types for chat conversation details in useChatStoreTypes file, including ChatConversationTypes and ChatHistoryTypes. These types include fields like sender, message, chatName, history, createdAt, and updatedAt.

--- client/src/types/useChatStoreTypes.ts
+++ client/src/types/useChatStoreTypes.ts
@@ -1,11 +1,30 @@
 export type ChatStoreStateTypes = {
-  history: string[];
+  conversationHistory: ChatHistoryTypes[];
 };
 
 export type ChatStoreActionTypes = {
-  newChat: (text: string) => Promise<void>;
+  newChat: (
+    userMessage: {sender: string; message: string},
+    id?: string,
+  ) => Promise<void>;
   continueChat: (text: string) => Promise<void>;
   getChatHistory: () => Promise<void>;
   getChatById: (id: string) => Promise<void>;
   deleteChat: (id: string) => Promise<void>;
+};
+
+export type ChatConversationTypes = {
+  _id?: string;
+  sender: string;
+  message: string;
+  translatedMessage?: string;
+};
+
+export type ChatHistoryTypes = {
+  __v: number;
+  _id: string;
+  chatName: string;
+  history: ChatConversationTypes[];
+  createdAt: Date;
+  updatedAt: Date;
 };
