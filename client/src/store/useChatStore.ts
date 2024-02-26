b2d12a9041ddd8b6aeffb6c39e43eab6665eb150
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-26 20:04:25 +0300

    🚀feat(useChatStore) Implemented newChat function using SSE for real-time chat handling.🚀
    This commit introduces changes to the useChatStore file, adding functionality to send user messages and receive responses using SSE technology. The newChat function now includes event listeners for opening connections, receiving messages, handling errors, and closing the connection when necessary. This update enhances the chat experience by enabling real-time communication with the server.

--- client/src/store/useChatStore.ts
+++ client/src/store/useChatStore.ts
@@ -7,44 +7,93 @@ import type {
 } from '../types/useChatStoreTypes';
 import axios from 'axios';
 import {SERVER_API_KEY, BASE_URL} from '@env';
+import EventSource, {
+  type CloseEvent,
+  type ErrorEvent,
+  type ExceptionEvent,
+  type MessageEvent,
+  type OpenEvent,
+  type TimeoutEvent,
+} from 'react-native-sse';
 
+interface ExtendedEventSource extends EventSource {
+  onmessage?: (event: MessageEvent) => void;
+  onopen?: (event: OpenEvent) => void;
+  onclose?: (event: CloseEvent) => void;
+  ontimeout?: (event: TimeoutEvent) => void;
+  onerror?: (error: Event) => void;
+}
 console.log('SERVER_API_KEY', SERVER_API_KEY);
 console.log('BASE_URL', BASE_URL);
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = BASE_URL;
 
 export const useChatStore = create(
   persist<ChatStoreStateTypes & ChatStoreActionTypes>(
     (set, get) => ({
-      history: [],
+      conversationHistory: [],
 
-      newChat: async text => {
-        try {
-          const response = await axios({
-            method: 'POST',
-            url: '/new',
-            data: {
-              message: text,
-            },
+      newChat: async (userMessage, setMessages, setLoading) => {
+        setLoading(true);
+
+        let newContent = '';
+        const eventSource: ExtendedEventSource = new EventSource(
+          `${BASE_URL}/new`,
+          {
             headers: {
+              'Content-Type': 'application/json',
               Authorization: `Bearer ${SERVER_API_KEY}`,
+              connection: 'keep-alive',
             },
-            responseType: 'stream',
-          });
+            method: 'POST',
+            body: JSON.stringify({
+              message: userMessage.message,
+            }),
+            pollingInterval: 25000,
+          },
+        );
 
-          response.data.on('data', (chunk: Buffer) => {
-            const botStream = chunk;
-            process.stdout.write(botStream);
-            console.log('botStream', botStream.toString());
-          });
+        const openListener = (event: OpenEvent) => {
+          if (event.type === 'open') {
+            console.log('Open SSE connection.');
+            setLoading(false);
+          } else {
+            console.log('error while opening SSE connection.');
+          }
+        };
 
-          response.data.on('end', () => {
-            console.log('Stream ended');
-          });
-          console.log('text', text);
-        } catch (error) {
-          console.error(error);
-        }
+        const messageListener = (event: MessageEvent) => {
+          if (event.data && event.data !== '[DONE]') {
+            const newWord = JSON.parse(event.data);
+            newContent = newContent + newWord;
+            // console.log(newContent);
+            setMessages((prev: string) => prev + newWord);
+          } else {
+            setLoading(false);
+            eventSource.close();
+          }
+        };
+
+        const errorListener = (
+          event: ErrorEvent | TimeoutEvent | ExceptionEvent,
+        ) => {
+          if ('data' in event) {
+            console.error('Connection error:', event.data);
+          } else if (event.type === 'error') {
+            console.error('Connection error:', event.message);
+          }
+          setLoading(false);
+          eventSource.close();
+        };
+
+        eventSource.addEventListener('open', openListener);
+        eventSource.addEventListener('message', messageListener);
+        eventSource.addEventListener('error', errorListener);
+
+        return () => {
+          eventSource.removeAllEventListeners();
+          eventSource.close();
+        };
       },
       continueChat: async text => {
         try {
@@ -56,32 +105,30 @@ export const useChatStore = create(
       getChatHistory: async () => {
         console.log('getChatHistory called');
         try {
-          // const {data} = await axios({
-          //   method: 'GET',
-          //   url: '/all',
-          //   headers: {
-          //     Accept: 'application/json',
-          //     'Content-Type': 'application/json',
-          //     Authorization: `Bearer ${SERVER_API_KEY}`,
-          //   },
-          // });
-
-          // console.log('data', data);
-
-          // set({
-          //   history: data,
-          // });
-
-          const response = await fetch(`${BASE_URL}/all`, {
+          const {data} = await axios({
             method: 'GET',
+            url: '/all',
             headers: {
+              Accept: 'application/json',
               'Content-Type': 'application/json',
               Authorization: `Bearer ${SERVER_API_KEY}`,
             },
           });
 
-          const data = await response.json();
-          console.log('data', data);
+          set({
+            conversationHistory:
+              data as ChatStoreStateTypes['conversationHistory'],
+          });
+
+          // const response = await fetch(`${BASE_URL}/all`, {
+          //   method: 'GET',
+          //   headers: {
+          //     'Content-Type': 'application/json',
+          //     Authorization: `Bearer ${SERVER_API_KEY}`,
+          //   },
+          // });
+
+          // const data = await response.json();
         } catch (error) {
           console.error(error);
         }
