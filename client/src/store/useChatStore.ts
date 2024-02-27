497af1f77499693c9e3ad164fca1b5e75b449f2f
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-27 19:03:37 +0300

    🚀feat(store) Added functionality for handling new and continued chat messages in useChatStore. 🚀feat(store) Added functionality for handling new and continued chat messages in useChatStore.

--- client/src/store/useChatStore.ts
+++ client/src/store/useChatStore.ts
@@ -23,6 +23,7 @@ interface ExtendedEventSource extends EventSource {
   ontimeout?: (event: TimeoutEvent) => void;
   onerror?: (error: Event) => void;
 }
+
 console.log('SERVER_API_KEY', SERVER_API_KEY);
 console.log('BASE_URL', BASE_URL);
 
@@ -32,11 +33,14 @@ export const useChatStore = create(
   persist<ChatStoreStateTypes & ChatStoreActionTypes>(
     (set, get) => ({
       conversationHistory: [],
+      userChat: [],
 
-      newChat: async (userMessage, setMessages, setLoading) => {
+      newChat: async (userMessage, setMessages, setLoading, id) => {
+        console.log();
+        const {conversationHistory, userChat} = get();
+        const index = conversationHistory.findIndex(chat => chat._id === id);
+
         setLoading(true);
-
-        let newContent = '';
         const eventSource: ExtendedEventSource = new EventSource(
           `${BASE_URL}/new`,
           {
@@ -62,13 +66,42 @@ export const useChatStore = create(
           }
         };
 
+        userChat.push({
+          _id: userMessage._id,
+          sender: userMessage.sender,
+          message: userMessage.message,
+        });
+
+        if (index !== -1) {
+          conversationHistory[index].history.push({
+            _id: userMessage._id,
+            sender: userMessage.sender,
+            message: userMessage.message,
+          });
+        }
+
+        let content = '';
+
         const messageListener = (event: MessageEvent) => {
           if (event.data && event.data !== '[DONE]') {
             const newWord = JSON.parse(event.data);
-            newContent = newContent + newWord;
-            // console.log(newContent);
+            content += newWord;
+
             setMessages((prev: string) => prev + newWord);
+
+            if (index !== -1) {
+              conversationHistory[index].history.push({
+                _id: new Date().toString(),
+                sender: 'bot',
+                message: content,
+              });
+            }
           } else {
+            userChat.push({
+              _id: new Date().toString(),
+              sender: 'bot',
+              message: content,
+            });
             setLoading(false);
             eventSource.close();
           }
@@ -95,12 +128,104 @@ export const useChatStore = create(
           eventSource.close();
         };
       },
-      continueChat: async text => {
-        try {
-          console.log('text', text);
-        } catch (error) {
-          console.error(error);
+      continueChat: async (userMessage, setMessages, setLoading, id) => {
+        if (!id) {
+          return () => {
+            console.log('id undefined');
+          };
         }
+        console.log('id', id);
+        const {conversationHistory, userChat} = get();
+        const index = conversationHistory.findIndex(chat => chat._id === id);
+
+        setLoading(true);
+        const eventSource: ExtendedEventSource = new EventSource(
+          `${BASE_URL}/chatbot`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${SERVER_API_KEY}`,
+              connection: 'keep-alive',
+            },
+            method: 'POST',
+            body: JSON.stringify({
+              id: id,
+              message: userMessage.message,
+            }),
+            pollingInterval: 25000,
+          },
+        );
+
+        const openListener = (event: OpenEvent) => {
+          if (event.type === 'open') {
+            console.log('Open SSE connection.');
+            setLoading(false);
+          } else {
+            console.log('error while opening SSE connection.');
+          }
+        };
+
+        userChat.push({
+          _id: userMessage._id,
+          sender: userMessage.sender,
+          message: userMessage.message,
+        });
+
+        if (index !== -1) {
+          conversationHistory[index].history.push({
+            _id: userMessage._id,
+            sender: userMessage.sender,
+            message: userMessage.message,
+          });
+        }
+
+        let content = '';
+
+        const messageListener = (event: MessageEvent) => {
+          if (event.data && event.data !== '[DONE]') {
+            const newWord = JSON.parse(event.data);
+            content += newWord;
+
+            setMessages((prev: string) => prev + newWord);
+
+            if (index !== -1) {
+              conversationHistory[index].history.push({
+                _id: new Date().toString(),
+                sender: 'bot',
+                message: content,
+              });
+            }
+          } else {
+            userChat.push({
+              _id: new Date().toString(),
+              sender: 'bot',
+              message: content,
+            });
+            setLoading(false);
+            eventSource.close();
+          }
+        };
+
+        const errorListener = (
+          event: ErrorEvent | TimeoutEvent | ExceptionEvent,
+        ) => {
+          if ('data' in event) {
+            console.error('Connection error:', event.data);
+          } else if (event.type === 'error') {
+            console.error('Connection error:', event.message);
+          }
+          setLoading(false);
+          eventSource.close();
+        };
+
+        eventSource.addEventListener('open', openListener);
+        eventSource.addEventListener('message', messageListener);
+        eventSource.addEventListener('error', errorListener);
+
+        return () => {
+          eventSource.removeAllEventListeners();
+          eventSource.close();
+        };
       },
       getChatHistory: async () => {
         console.log('getChatHistory called');
