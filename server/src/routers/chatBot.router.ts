c8619a4a8eb46882b216a4e30e65c86ab4bebbe2
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-02-28 17:54:05 +0300

    🛠️refactor(chatBot): Removed unnecessary streaming logic in chatBot router

--- server/src/routers/chatBot.router.ts
+++ server/src/routers/chatBot.router.ts
@@ -21,26 +21,6 @@ BotRouter.post('/chat/continueChat', continueChat)
   .get('/all', getAllChatHistory)
   .get('/chat/:id', getChatById)
   .delete('/delete/:id', deleteChat)
-  .delete('/delete/message/:id', deleteMessage)
-  .get('/stream', (req, res) => {
-    console.log('Streaming...');
-    res.writeHead(200, {
-      'Content-Type': 'text/event-stream',
-      Connection: 'keep-alive',
-      'Cache-Control': 'no-cache',
-    });
-
-    // Example of sending a message every second
-    let count = 0;
-    const interval = setInterval(() => {
-      res.write(`data: ${count++}\n\n`);
-      console.log(count);
-      if (count > 7) {
-        clearInterval(interval);
-        console.log('Done!');
-        res.write('data: [DONE]\n\n');
-      }
-    }, 1000);
-  });
+  .delete('/delete/message/:id', deleteMessage);
 
 export default BotRouter;
