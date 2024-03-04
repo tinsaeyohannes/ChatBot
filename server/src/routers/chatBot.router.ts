7f9439e84c521fa7fcc78dae7085bc7e1a57ae24
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-04 14:36:04 +0300

    🚀feat(chatBot): Remove unnecessary chat functionalities and image generation
    Removed newChatStream, newChat, continueChatStream, continueChat, and generateImages endpoints from the BotRouter in chatBot.router.ts. Refactored the code to only include getAllChatHistory, getChatById, deleteChat, and deleteMessage endpoints.

--- server/src/routers/chatBot.router.ts
+++ server/src/routers/chatBot.router.ts
@@ -1,24 +1,14 @@
 import express, { Router } from 'express';
 import {
-  newChatStream,
-  newChat,
-  continueChatStream,
-  continueChat,
   deleteChat,
   getAllChatHistory,
   getChatById,
   deleteMessage,
 } from '../controllers/chatBot.controller';
-import { generateImages } from '../controllers/imageGenerator.controller';
 
 const BotRouter: Router = express.Router();
 
-BotRouter.post('/chat/continueChat', continueChat)
-  .post('/chat/continueChatStream', continueChatStream)
-  .post('/chat/newChat', newChat)
-  .post('/chat/newChatStream', newChatStream)
-  .post('/generateImg', generateImages)
-  .get('/all', getAllChatHistory)
+BotRouter.get('/all', getAllChatHistory)
   .get('/chat/:id', getChatById)
   .delete('/delete/:id', deleteChat)
   .delete('/delete/message/:id', deleteMessage);
