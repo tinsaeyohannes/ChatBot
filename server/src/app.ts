6bfc8c2d9c68134c8cc1fee9db3b65d8b73c232b
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-04 20:59:19 +0300

    🚀feat(api): Add new AI chat routes for Gemini, OpenAI, and Cohere in the server app; allow user to access AI services through respective endpoints.

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,9 @@ import BotRouter from './routers/chatBot.router';
 import morgan from 'morgan';
 import apiKeyMiddleware from './middleware/ApiKeyMiddleWare';
 import cors from 'cors';
+import GeminiRouter from './routers/ai-chat-routes/gemini.router';
+import OpenAIRouter from './routers/ai-chat-routes/openAI.router';
+import CohereRouter from './routers/ai-chat-routes/cohere.router';
 
 const app: Express = express();
 
@@ -16,5 +19,8 @@ app.use(express.json());
 app.use(apiKeyMiddleware);
 
 app.use('/api', BotRouter);
+app.use('/api/openai', OpenAIRouter);
+app.use('/api/gemini', GeminiRouter);
+app.use('/api/cohere', CohereRouter);
 
 export default app;
