d98779e6b0cae323f49e217f15e22223c219b527
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-04 13:12:25 +0300

    🚀feat(ai-chat): Updated history model and added new chat functionality for Gemini AI. - Updated import of GeminiHistoryModel in ai-chat-models schema. - Renamed gemini function to geminiNewChat for clarity and consistency. - Added geminiContinueChat function for continuing chats with previous history.

--- server/src/controllers/ai-chat-models/gemini.ts
+++ server/src/controllers/ai-chat-models/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 import { Request, Response } from 'express';
-import HistoryModel from '../../models/ConversationHistory.mongo';
+import GeminiHistoryModel from '../../models/ai-chat-models-schema/GeminiHistoryModel.mongo';
 
 const genAIInit = new GoogleGenerativeAI(`${process.env.GEMINI_API_KEY}`);
 
@@ -25,7 +25,7 @@ const addChatName = async (userMessage: string, botMessage: string) => {
     console.error(error);
   }
 };
-async function gemini(req: Request, res: Response) {
+async function geminiNewChat(req: Request, res: Response) {
   const { prompt } = req.body;
   try {
     if (!prompt) {
@@ -34,7 +34,6 @@ async function gemini(req: Request, res: Response) {
       });
     }
 
-    // For text-only input, use the gemini-pro model
     const model = genAIInit.getGenerativeModel({
       model: 'gemini-pro',
     });
@@ -43,32 +42,31 @@ async function gemini(req: Request, res: Response) {
 
     if (geminiResult && geminiResult.response) {
       const geminiResultText = geminiResult.response;
-      if (geminiResultText.candidates) {
-        const botMessage = geminiResultText.candidates[0].content.parts[0].text;
 
-        if (botMessage) {
-          const chatName = await addChatName(prompt, botMessage);
+      const botMessage = geminiResultText.text();
 
-          const newChat = new HistoryModel({
-            chatName: chatName,
-            botName: 'gemini',
-            history: [
-              {
-                sender: 'user',
-                message: prompt,
-              },
-            ],
-          });
+      if (botMessage) {
+        const chatName = await addChatName(prompt, botMessage);
 
-          newChat.history.push({
-            sender: 'bot',
-            message: botMessage,
-          });
+        const newChat = new GeminiHistoryModel({
+          chatName: chatName,
+          botName: 'gemini',
+          history: [
+            {
+              sender: 'user',
+              message: prompt,
+            },
+          ],
+        });
 
-          const response = await newChat.save();
+        newChat.history.push({
+          sender: 'bot',
+          message: botMessage,
+        });
 
-          res.status(200).json(response);
-        }
+        const response = await newChat.save();
+
+        res.status(200).json(response);
       }
     }
   } catch (err) {
@@ -78,5 +76,68 @@ async function gemini(req: Request, res: Response) {
     });
   }
 }
+async function geminiContinueChat(req: Request, res: Response) {
+  const { prompt, chatId } = req.body;
+  try {
+    if (!prompt) {
+      return res.json({
+        error: 'no prompt',
+      });
+    }
 
-export { gemini };
+    const model = genAIInit.getGenerativeModel({
+      model: 'gemini-pro',
+    });
+
+    const conversationHistory = await GeminiHistoryModel.findById(chatId);
+
+    if (!conversationHistory) {
+      res.status(404).json('Empty History!');
+      return;
+    }
+
+    const chatHistory = conversationHistory?.history.map((doc) => ({
+      role: doc.sender === 'user' ? 'user' : 'model',
+      parts: Array.isArray(doc.message) ? doc.message : [doc.message || ''],
+    }));
+
+    const chat = model.startChat({
+      history: chatHistory,
+      generationConfig: {
+        maxOutputTokens: 1000,
+      },
+    });
+
+    const result = await chat.sendMessage(prompt);
+
+    console.log('result', result.response.text());
+    const history = await chat.getHistory();
+    console.log('history', history);
+    history.forEach((item) => console.log(item));
+
+    const botMessage = result.response.text();
+
+    if (botMessage) {
+      conversationHistory.history.push({
+        sender: 'user',
+        message: prompt,
+      });
+
+      conversationHistory.history.push({
+        sender: 'bot',
+        message: botMessage,
+      });
+
+      const response = await conversationHistory.save();
+
+      res.status(200).json(response);
+    }
+  } catch (err) {
+    console.log('error in Gemini chat: ', err);
+    res.status(500).json({
+      message: (err as Error).message,
+    });
+  }
+}
+
+export { geminiNewChat, geminiContinueChat };
