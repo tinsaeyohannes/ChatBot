04e80be3657f20dda827ae46ce9ec62a70d2c071
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-05 22:25:08 +0300

    🚀feat(server): Add support for image routes in the AI application; updated express.json to handle larger payloads.

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import GeminiRouter from './routers/ai-chat-routes/gemini.router';
 import OpenAIRouter from './routers/ai-chat-routes/openAI.router';
 import CohereRouter from './routers/ai-chat-routes/cohere.router';
+import ImagesRouter from './routers/ai-image-routes/images.router';
 
 const app: Express = express();
 
@@ -13,7 +14,7 @@ app.use(cors());
 
 app.use(morgan('tiny'));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Apply API key authentication middleware to all routes
 app.use(apiKeyMiddleware);
@@ -22,5 +23,6 @@ app.use('/api', BotRouter);
 app.use('/api/openai', OpenAIRouter);
 app.use('/api/gemini', GeminiRouter);
 app.use('/api/cohere', CohereRouter);
+app.use('/api/image', ImagesRouter);
 
 export default app;
