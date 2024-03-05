e5f608e2baf0f33bd8c1666b4c1b53c5c784c24a
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-05 22:22:02 +0300

    🚀feat(profile) Added navigation buttons and UI improvements to Profile screen.

--- client/src/screens/ProfileScreen.tsx
+++ client/src/screens/ProfileScreen.tsx
@@ -1,14 +1,28 @@
 import React, {type FC} from 'react';
-import {StatusBar, StyleSheet, Text} from 'react-native';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {userStore} from '../store/useStore';
-import type {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import type {ParamListBase} from '@react-navigation/native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import type {DrawerNavigationProp} from '@react-navigation/drawer';
+import {
+  // widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 type ProfileScreenProps = {
-  navigation: NativeStackNavigationProp<ParamListBase>;
+  navigation: DrawerNavigationProp<ParamListBase>;
 };
-const ProfileScreen: FC<ProfileScreenProps> = ({}): React.JSX.Element => {
+const ProfileScreen: FC<ProfileScreenProps> = ({
+  navigation,
+}): React.JSX.Element => {
   const {isDarkMode} = userStore(state => ({
     isDarkMode: state.isDarkMode,
   }));
@@ -19,7 +33,28 @@ const ProfileScreen: FC<ProfileScreenProps> = ({}): React.JSX.Element => {
         isDarkMode ? styles.darkBg : styles.lightBg,
       ]}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>ProfileScreen</Text>
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.headerButton}
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <AntDesign
+            name="left"
+            size={24}
+            color={isDarkMode ? 'white' : 'black'}
+          />
+        </TouchableOpacity>
+
+        <Text>Settings</Text>
+
+        <Button
+          title="image"
+          onPress={() => {
+            navigation.navigate('Image');
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -27,15 +62,26 @@ const ProfileScreen: FC<ProfileScreenProps> = ({}): React.JSX.Element => {
 const styles = StyleSheet.create({
   safeAreaViewContainer: {
     flex: 1,
-
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   darkBg: {
     backgroundColor: '#031C1A',
   },
   lightBg: {
     backgroundColor: '#E5E5E5',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerButton: {
+    width: hp(6),
+    height: hp(6),
+    margin: hp(1.5),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    backgroundColor: '#006570',
   },
 });
 
