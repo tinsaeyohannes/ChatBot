b6c483aef08721f0420c6bd3aee81153b8c237ca
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-06 20:10:36 +0300

    🛠️refactor(router): Removed unnecessary route and controller method in chatBot router;

--- server/src/routers/chatBot.router.ts
+++ server/src/routers/chatBot.router.ts
@@ -2,15 +2,12 @@ import express, { Router } from 'express';
 import {
   deleteChat,
   getAllChatHistory,
-  getChatById,
-  deleteMessage,
 } from '../controllers/chatBot.controller';
 
 const BotRouter: Router = express.Router();
 
 BotRouter.get('/all', getAllChatHistory)
-  .get('/chat/:id', getChatById)
-  .delete('/delete/:id', deleteChat)
-  .delete('/delete/message/:id', deleteMessage);
+
+  .delete('/delete/:id', deleteChat);
 
 export default BotRouter;
