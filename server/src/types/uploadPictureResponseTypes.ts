63f30180fd3af195c79552cb5b8434f8c50cebf3
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-06 20:10:48 +0300

    🚀feat(server): Added new types for uploading picture response
    🚀feat(server): Introduced interfaces and types for upload picture response to include secure URL, public ID, and FAL response details.

---
+++ server/src/types/uploadPictureResponseTypes.ts
@@ -0,0 +1,27 @@
+export interface UploadPictureResponse {
+  secureUrl: string;
+  publicId: string;
+}
+
+export type FalResponseImage = {
+  content_type: string;
+  height: number;
+  url: string;
+  width: number;
+};
+export type FalResponseTypes = {
+  has_nsfw_concepts: boolean[];
+  images: FalResponseImage[];
+  seed: number;
+  timings: {
+    inference: number;
+  };
+};
+export type FalResponseTypes2 = {
+  image: {
+    height: number;
+    content_type: string;
+    url: string;
+    width: number;
+  };
+};
