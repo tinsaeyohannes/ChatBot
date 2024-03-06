03a9c01b99493370fc0934b790f67ca2e7029623
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-06 20:08:20 +0300

    📝 Add or update documentation.
    - Import the `uploadPicture` function from the `imageUpload` service.
    - Import the `UploadPictureResponse`, `FalResponseTypes`, and `FalResponseTypes2` types from the `uploadPictureResponseTypes.
    - Add a check to ensure that the `model` parameter is provided.
    - Log the value of the `model` parameter to the console.
    
    - Update the `fal` function to handle different types of AI models.
    - If the model is a text-to-image model, use the `falClient.subscribe` method to generate an image based on the input prompt and negative prompt.
    - If the model is an image-to-image model, use the `uploadPicture` function to upload the image and retrieve the secure URL.
    - Use the `falClient.subscribe` method to generate an image based on the input image URL and prompt.
    
    - Return the generated image URL in the response.
    
    - Handle any errors that occur during the process and return an appropriate error message in the response.

--- server/src/controllers/ai-image-models/fal.ts
+++ server/src/controllers/ai-image-models/fal.ts
@@ -1,25 +1,78 @@
+import { uploadPicture } from '../../services/imageUpload';
 import type { Request, Response } from 'express';
+import {
+  UploadPictureResponse,
+  type FalResponseTypes,
+  type FalResponseTypes2,
+} from '../../types/uploadPictureResponseTypes';
+import * as falClient from '@fal-ai/serverless-client';
+import { Models } from '../../constants/models';
+
+falClient.config({
+  credentials: process.env.FAL_API_KEY,
+});
+
+const negative_prompt = 'ugly, deformed';
 
 const fal = async (req: Request, res: Response) => {
-  const { image }: { prompt: string; image: string } = req.body;
-
-  if (!image) {
+  const {
+    image,
+    prompt,
+    model,
+  }: { prompt: string; image: string; model: string } = req.body;
+  if (!model) {
     res.status(400).json({
-      error: 'Please upload an image',
-    });
-    return;
-  } else {
-    console.log('image', image);
-
-    res.status(200).json({
-      message: 'image has successfully uploaded',
+      error: 'Please specify model',
     });
     return;
   }
+  console.log('model', model);
+
   try {
+    const updatedImage = `data:image/png;base64,${image}`;
+
+    for (const m of Models) {
+      if (model === m.label && m.modelType === 'txt2img') {
+        const falResponse: FalResponseTypes = await falClient.subscribe(
+          m.modelName,
+          {
+            input: {
+              prompt:
+                '(masterpiece:1.4), (best quality), (detailed), ' + prompt,
+              negative_prompt,
+            },
+          },
+        );
+
+        res.status(200).json({ image: falResponse.images[0].url });
+        return;
+      } else if (model === m.label && m.modelType === 'img2img') {
+        const { secureUrl } = (await uploadPicture(
+          updatedImage,
+        )) as UploadPictureResponse;
+
+        const falResponse: FalResponseTypes & FalResponseTypes2 =
+          await falClient.subscribe(m.modelName, {
+            input: {
+              image_url: secureUrl,
+              prompt: prompt,
+            },
+          });
+
+        if (falResponse.images) {
+          res.status(200).json({ image: falResponse.images[0].url });
+        } else if (falResponse.image) {
+          res.status(200).json({ image: falResponse.image.url });
+        } else {
+          res
+            .status(500)
+            .json({ error: 'No images returned from the AI model' });
+        }
+      }
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: (error as Error).message });
   }
 };
 
