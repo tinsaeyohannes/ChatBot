115132d8ec1824ea9800816dff54571544be3afb
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-07 22:15:20 +0300

    🚀feat(images.router) Updated createImage routes in AI image models controllers - Refactored generateImages function to generateImage and added continueToGenerateImages - Added new route for continueCreateImage in DALL-E model - Organized the createImage routes for DALL-E and FAL models in the ImagesRouter

--- server/src/routers/ai-image-routes/images.router.ts
+++ server/src/routers/ai-image-routes/images.router.ts
@@ -1,12 +1,14 @@
 import { fal } from '../../controllers/ai-image-models/fal';
-import { generateImages } from '../../controllers/ai-image-models/dall-e';
+import {
+  continueToGenerateImages,
+  generateImage,
+} from '../../controllers/ai-image-models/dall-e';
 import express, { Router } from 'express';
 
 const ImagesRouter: Router = express.Router();
 
-ImagesRouter.post('/dalle/createImage', generateImages).post(
-  '/fal/createImage',
-  fal,
-);
+ImagesRouter.post('/dalle/createImage', generateImage)
+  .post('/dalle/continueCreateImage', continueToGenerateImages)
+  .post('/fal/createImage', fal);
 
 export default ImagesRouter;
