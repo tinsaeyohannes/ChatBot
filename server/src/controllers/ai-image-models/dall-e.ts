f843cc77c242c0e73ac361554606cfed2906446d
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-07 22:15:13 +0300

    🚀feat(ai-image-models) Updated image generation endpoint to handle continuation of image generation from a previous chat; created new function continueToGenerateImages.

--- server/src/controllers/ai-image-models/dall-e.ts
+++ server/src/controllers/ai-image-models/dall-e.ts
@@ -7,7 +7,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const generateImages = async (req: Request, res: Response) => {
+const generateImage = async (req: Request, res: Response) => {
   const { prompt }: { prompt: string } = req.body;
   try {
     if (!prompt) {
@@ -57,5 +57,64 @@ const generateImages = async (req: Request, res: Response) => {
     });
   }
 };
+const continueToGenerateImages = async (req: Request, res: Response) => {
+  const { chatId, prompt }: { chatId: string; prompt: string } = req.body;
+  try {
+    if (!chatId) {
+      res.status(400).json({
+        error: 'Chat ID Required!',
+      });
+      return;
+    }
+    if (!prompt) {
+      res.status(400).json({
+        error: 'Please enter a prompt',
+      });
+      return;
+    }
+    const chat = await ImageHistoryModel.findById(chatId);
+    if (!chat) {
+      res.status(404).json({
+        error: 'Chat not found',
+      });
+      return;
+    }
+    const response = await openai.images.generate({
+      model: 'dall-e-2',
+      prompt: `I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: ${prompt}`,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'url',
+      style: 'vivid',
+      quality: 'hd',
+    });
 
-export { generateImages };
+    const imageData = response.data[0].url;
+
+    console.log('imageData', imageData);
+
+    if (imageData) {
+      chat.history.push({
+        sender: 'user',
+        prompt: prompt,
+      });
+
+      chat.history.push({
+        sender: 'model',
+        generated_Image: imageData,
+      });
+
+      const response = await chat.save();
+
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    console.error((error as Error).message);
+    res.status(500).json({
+      error: (error as Error).message,
+      message: 'Internal Server Error!',
+    });
+  }
+};
+
+export { generateImage, continueToGenerateImages };
