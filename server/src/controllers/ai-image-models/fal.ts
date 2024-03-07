d85a9d263df48e6f5f3a510bc211ee7877ab0530
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-07 22:44:23 +0300

    🚀feat(ai-image-models) Renamed fal function to generateWithFal and continueWithfal function for handling chat interactions and model responses.

--- server/src/controllers/ai-image-models/fal.ts
+++ server/src/controllers/ai-image-models/fal.ts
@@ -15,7 +15,7 @@ falClient.config({
 
 const negative_prompt = 'ugly, deformed';
 
-const fal = async (req: Request, res: Response) => {
+const generateWithFal = async (req: Request, res: Response) => {
   const {
     image,
     prompt,
@@ -123,4 +123,118 @@ const fal = async (req: Request, res: Response) => {
   }
 };
 
-export { fal };
+const continueWithfal = async (req: Request, res: Response) => {
+  const {
+    chatId,
+    image,
+    prompt,
+    model,
+  }: { chatId: string; prompt: string; image: string; model: string } =
+    req.body;
+
+  if (!model) {
+    res.status(400).json({
+      error: 'Please specify model',
+    });
+    return;
+  }
+
+  console.log('model', model);
+
+  try {
+    const chat = await ImageHistoryModel.findById(chatId);
+
+    if (!chat) {
+      res.status(404).json({
+        error: 'Chat not found',
+      });
+      return;
+    }
+
+    const updatedImage = `data:image/png;base64,${image}`;
+
+    for (const m of Models) {
+      if (model === m.label && m.modelType === 'txt2img') {
+        const falResponse: FalResponseTypes = await falClient.subscribe(
+          m.modelName,
+          {
+            input: {
+              prompt:
+                '(masterpiece:1.4), (best quality), (detailed), ' + prompt,
+              negative_prompt,
+            },
+          },
+        );
+
+        chat.history.push({
+          sender: 'user',
+          prompt: prompt,
+        });
+
+        chat.history.push({
+          sender: 'model',
+          generated_Image: falResponse.images[0].url,
+        });
+
+        const response = await chat.save();
+        res.status(200).json(response);
+        return;
+      } else if (model === m.label && m.modelType === 'img2img') {
+        const { secureUrl } = (await uploadPicture(
+          updatedImage,
+        )) as UploadPictureResponse;
+
+        const falResponse: FalResponseTypes & FalResponseTypes2 =
+          await falClient.subscribe(m.modelName, {
+            input: {
+              image_url: secureUrl,
+              prompt: prompt,
+            },
+          });
+
+        chat.history.push({
+          sender: 'user',
+          prompt: prompt,
+        });
+
+        chat.history.push({
+          sender: 'model',
+          generated_Image: falResponse.images[0].url,
+        });
+
+        const response = await chat.save();
+        res.status(200).json(response);
+
+        if (falResponse.images) {
+          chat.history.push({
+            sender: 'model',
+            generated_Image: falResponse.images[0].url,
+          });
+
+          const response = await chat.save();
+          res.status(200).json(response);
+          return;
+        } else if (falResponse.image) {
+          chat.history.push({
+            sender: 'model',
+            generated_Image: falResponse.image.url,
+          });
+
+          const response = await chat.save();
+          res.status(200).json(response);
+
+          return;
+        } else {
+          res
+            .status(500)
+            .json({ error: 'No images returned from the AI model' });
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
+
+export { generateWithFal, continueWithfal };
