1146859810dc9d999d1cc39d775a7a72ba57806c
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-08 22:11:12 +0300

    🚀feat(store) Add uploadImage function and model, prompt parameters useImageStore ️refactor(store) Update image upload endpoint URL 🚀feat(store) Add getAllImageHistories function in useImageStore

--- client/src/store/useImageStore.ts
+++ client/src/store/useImageStore.ts
@@ -1,28 +1,44 @@
 import {create} from 'zustand';
 import {createJSONStorage, persist} from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {BASE_URL, SERVER_API_KEY} from '@env';
+import type {
+  UseImageStoreActionsTypes,
+  UseImageStoreStateTypes,
+} from '../types/useImageStoreTypes';
 
 export const useImageStore = create(
-  persist(
-    (set, get) => ({
-      image: '',
+  persist<UseImageStoreStateTypes & UseImageStoreActionsTypes>(
+    set => ({
+      imagesHistory: [],
+      currentChat: null,
 
-      uploadImage: async (image: string) => {
+      uploadImage: async (image, model, prompt) => {
         try {
-          const response = await fetch(`${BASE_URL}/image/dalle/createImage`, {
+          const response = await fetch(`${BASE_URL}/image/fal/createImage`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
               Authorization: `Bearer ${SERVER_API_KEY}`,
             },
+
             body: JSON.stringify({
-              image,
+              image: image,
+              model: model,
+              prompt: prompt,
             }),
           });
 
           const data = await response.json();
+          set({imagesHistory: data});
           console.log(data);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      getAllImageHistories: async () => {
+        try {
         } catch (error) {
           console.error(error);
         }
