47d790d3bcd163b6875da5a960fabc21501a4417
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-08 22:14:26 +0300

    🚀feat(App): Added GestureHandlerRootView for gesture handling in navigation container. Updated styles accordingly.

--- client/App.tsx
+++ client/App.tsx
@@ -8,10 +8,11 @@ import SystemNavigationBar from 'react-native-system-navigation-bar';
 import ChatScreen from './src/screens/ChatScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import DrawerBarContent from './src/drawerBar/DrawerBarContent';
-import {useColorScheme} from 'react-native';
+import {StyleSheet, useColorScheme} from 'react-native';
 import {userStore} from './src/store/useStore';
 import * as Sentry from '@sentry/react-native';
 import ImageGenerateScreen from './src/screens/ImageGenerateScreen';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 Sentry.init({
   dsn: 'https://cc59228a70f3cb8927602390400d772a@o4506451331579904.ingest.us.sentry.io/4506451334004736',
@@ -49,55 +50,62 @@ function App() {
   }, [isDark]);
 
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Chat"
-        screenOptions={{
-          drawerType: 'back',
-          headerStyle: {
-            backgroundColor: 'blue',
-            shadowColor: 'transparent',
-            elevation: 0,
-            borderBottomWidth: 0,
-          },
+    <GestureHandlerRootView style={styles.GestureHandlerRootView}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          initialRouteName="Chat"
+          screenOptions={{
+            drawerType: 'back',
+            headerStyle: {
+              backgroundColor: 'blue',
+              shadowColor: 'transparent',
+              elevation: 0,
+              borderBottomWidth: 0,
+            },
 
-          headerTintColor: 'white',
-          headerTransparent: true,
-          keyboardDismissMode: 'on-drag',
-          drawerStyle: {
-            backgroundColor: '#fff',
-          },
-        }}
-        drawerContent={DrawerBarContentComponent}>
-        <Drawer.Screen
-          name="Chat"
-          component={ChatScreen}
-          options={{
-            title: 'Chat Bot',
-            headerShown: false,
+            headerTintColor: 'white',
+            headerTransparent: true,
+            keyboardDismissMode: 'on-drag',
+            drawerStyle: {
+              backgroundColor: '#fff',
+            },
           }}
-        />
-        <Drawer.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            swipeEnabled: false,
-            headerShown: false,
-            drawerLabel: 'Profile',
-          }}
-        />
-        <Drawer.Screen
-          name="Image"
-          component={ImageGenerateScreen}
-          options={{
-            swipeEnabled: false,
-            headerShown: false,
-            drawerLabel: 'Profile',
-          }}
-        />
-      </Drawer.Navigator>
-    </NavigationContainer>
+          drawerContent={DrawerBarContentComponent}>
+          <Drawer.Screen
+            name="Chat"
+            component={ChatScreen}
+            options={{
+              title: 'Chat Bot',
+              headerShown: false,
+            }}
+          />
+          <Drawer.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              swipeEnabled: false,
+              headerShown: false,
+              drawerLabel: 'Profile',
+            }}
+          />
+          <Drawer.Screen
+            name="Image"
+            component={ImageGenerateScreen}
+            options={{
+              swipeEnabled: false,
+              headerShown: false,
+              drawerLabel: 'Profile',
+            }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
+const styles = StyleSheet.create({
+  GestureHandlerRootView: {
+    flex: 1,
+  },
+});
 export default App;
