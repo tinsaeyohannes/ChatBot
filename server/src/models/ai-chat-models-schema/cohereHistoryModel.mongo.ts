df0eb2a48a397c3bdde3c8d68030a09c6a790cbb
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-09 20:28:30 +0300

    ⚙️chore(models): Add createdAt field in the CohereHistorySchema in cohereHistoryModel.mongo.ts

--- server/src/models/ai-chat-models-schema/cohereHistoryModel.mongo.ts
+++ server/src/models/ai-chat-models-schema/cohereHistoryModel.mongo.ts
@@ -20,6 +20,10 @@ const CohereHistorySchema: Schema = new mongoose.Schema(
           type: String || null,
           required: true,
         },
+        createdAt: {
+          type: Date,
+          default: Date.UTC,
+        },
       },
     ],
   },
