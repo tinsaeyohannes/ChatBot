f25b489a463d041cf6f46f6fae294abb6786982e
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-10 23:50:46 +0300

    🚀feat(constants): Add ImageModels types and data Added a file `ImageModels` with type definitions image models and initial data for various image models including their IDs, names, and model types.

---
+++ client/src/constants/ImageModels.ts
@@ -0,0 +1,62 @@
+export type ImageModelTypes = {
+  id: number;
+  name: string;
+  modelType: string;
+};
+export const ImageModels: ImageModelTypes[] = [
+  {
+    id: 1,
+    name: 'DALL-E',
+    modelType: 'text2img',
+  },
+  {
+    id: 2,
+    name: 'Fast Image',
+    modelType: 'text2img',
+  },
+  {
+    id: 3,
+    name: 'Stable Diffusion XL Lightning',
+    modelType: 'text2img',
+  },
+  {
+    id: 4,
+    name: 'Stable DiffusionXL',
+    modelType: 'text2img',
+  },
+  {
+    id: 5,
+    name: 'Esrgan Upscale',
+    modelType: 'img2img',
+  },
+  {
+    id: 6,
+    name: 'CCSRUpscaler',
+    modelType: 'img2img',
+  },
+  {
+    id: 7,
+    name: 'Face Retoucher',
+    modelType: 'img2img',
+  },
+  {
+    id: 8,
+    name: 'Creative Upscaler',
+    modelType: 'img2img',
+  },
+  {
+    id: 9,
+    name: 'Remove Background',
+    modelType: 'img2img',
+  },
+  {
+    id: 10,
+    name: 'Stable Cascade',
+    modelType: 'txt2img',
+  },
+  {
+    id: 11,
+    name: 'Supir Upscaler',
+    modelType: 'img2img',
+  },
+];
