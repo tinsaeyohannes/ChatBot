08613211b1c31be3dce18ad6f86c0d956898a1de
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-11 21:03:50 +0300

    🚀feat(ModelTypes) Added ImageModelTypes and TextTypes to define types different models

---
+++ client/src/types/ModelTypes.ts
@@ -0,0 +1,13 @@
+export type ImageModelTypes = {
+  id: number;
+  name: string;
+  modelType: string;
+  backgroundImage: string;
+  provider: string;
+};
+export type TextModelTypes = {
+  id: number;
+  modelType: string;
+  endPoint: string;
+  name: string;
+};
