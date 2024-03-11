282d0ac85e64b97f77f7b00a41df48682c785db4
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-11 21:02:38 +0300

    🚀feat(store): Added new userMessage state and setUserMessage function; Added generateImage function to create image based on model and prompt; Modified uploadImage function to dynamically set currentModel when creating image using fetch API; getAllImageHistories function to fetch and set all image histories.

--- client/src/store/useImageStore.ts
+++ client/src/store/useImageStore.ts
@@ -4,41 +4,102 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {BASE_URL, SERVER_API_KEY} from '@env';
 import type {
+  ImagesHistoryTypes,
   UseImageStoreActionsTypes,
   UseImageStoreStateTypes,
 } from '../types/useImageStoreTypes';
 
 export const useImageStore = create(
   persist<UseImageStoreStateTypes & UseImageStoreActionsTypes>(
-    set => ({
+    (set, get) => ({
       imagesHistory: [],
       currentChat: null,
+      userMessage: '',
+      currentModel: '',
+      setUserMessage: (message: string) => set({userMessage: message}),
 
-      uploadImage: async (image, model, prompt) => {
+      generateImage: async (model, prompt, setLoading) => {
+        const {currentModel} = get();
+
+        if (!currentModel) {
+          console.log('model is undefined');
+          return;
+        }
+        setLoading(true);
+
         try {
-          const response = await fetch(`${BASE_URL}/image/fal/createImage`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${SERVER_API_KEY}`,
-            },
+          const response = await fetch(
+            `${BASE_URL}/image/${currentModel}/createImage`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${SERVER_API_KEY}`,
+              },
 
-            body: JSON.stringify({
-              image: image,
-              model: model,
-              prompt: prompt,
-            }),
-          });
+              body: JSON.stringify({
+                model: model,
+                prompt: prompt,
+              }),
+            },
+          );
 
           const data = await response.json();
           set({imagesHistory: data});
           console.log(data);
+          setLoading(false);
         } catch (error) {
+          setLoading(false);
+          console.error(error);
+        }
+      },
+      uploadImage: async (image, model, prompt, setLoading) => {
+        const {currentModel} = get();
+
+        if (!currentModel) {
+          console.log('model is undefined');
+          return;
+        }
+        setLoading(true);
+        try {
+          const response = await fetch(
+            `${BASE_URL}/image/${currentModel}/createImage`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${SERVER_API_KEY}`,
+              },
+
+              body: JSON.stringify({
+                image: image,
+                model: model,
+                prompt: prompt,
+              }),
+            },
+          );
+
+          const data = await response.json();
+          set({imagesHistory: data});
+          console.log(data);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
           console.error(error);
         }
       },
       getAllImageHistories: async () => {
         try {
+          const response = await fetch(`${BASE_URL}/image/getImagesHistory`, {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${SERVER_API_KEY}`,
+            },
+          });
+
+          const data = await response.json();
+
+          set({imagesHistory: data as ImagesHistoryTypes[]});
         } catch (error) {
           console.error(error);
         }
