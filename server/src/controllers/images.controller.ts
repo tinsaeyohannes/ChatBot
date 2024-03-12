459a51bb6e726791ded150a7c25e17b4b549b8d5
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-12 22:18:23 +0300

    🛠️refactor(images.controller): Removed unnecessary code and commented out; removed console log for model and imageHistory; cleaned up code for better readability

--- server/src/controllers/images.controller.ts
+++ server/src/controllers/images.controller.ts
@@ -2,19 +2,9 @@ import type { Request, Response } from 'express';
 import ImageHistoryModel from '../models/ai-image-models-schema/imageModel.mongo';
 
 const getAllImageHistories = async (req: Request, res: Response) => {
-  const model = req.query.model;
   const page = parseInt(req.query.page as string) || 1;
   const limit = parseInt(req.query.limit as string) || 10;
   const skip = (page - 1) * limit;
-
-  console.log('model', model);
-
-  // if (!model) {
-  //   res.status(400).json({
-  //     message: 'Please enter a model',
-  //   });
-  //   return;
-  // }
 
   try {
     const imageHistory = await ImageHistoryModel.find().skip(skip).limit(limit);
@@ -25,8 +15,6 @@ const getAllImageHistories = async (req: Request, res: Response) => {
       });
       return;
     }
-
-    console.log('imageHistory', imageHistory);
 
     res.status(200).json(imageHistory);
   } catch (error) {
