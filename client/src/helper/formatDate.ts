545d23e9899bd6f82440f57d9667a9134c435873
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-03-14 22:24:16 +0300

    ⚙️chore(formatDate.ts): Updated formatDate function to use Moment.js for relative time formatting

--- client/src/helper/formatDate.ts
+++ client/src/helper/formatDate.ts
@@ -1,5 +1,14 @@
-import {formatDistanceToNow} from 'date-fns';
+// import {formatDistanceToNow} from 'date-fns';
+
+// export const formatDate = (date: Date) => {
+//   return formatDistanceToNow(date, {addSuffix: false});
+// };
+
+import moment from 'moment';
 
 export const formatDate = (date: Date) => {
-  return formatDistanceToNow(date, {addSuffix: false});
+  // Convert the date to a Moment.js object
+  const momentDate = moment(date);
+  // Format the date as relative time
+  return momentDate.fromNow();
 };
